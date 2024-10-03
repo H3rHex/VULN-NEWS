41bc232b5bb25fb63881b70ab1a653a8c66c4599
H3rHex <149667352+H3rHex@users.noreply.github.com>
2024-10-03 13:25:10 +0200

    HEADER

---
+++ JS/global.js
@@ -0,0 +1,23 @@
+//SCRIPT GENERAL
+
+document.addEventListener("DOMContentLoaded", () =>{
+    updateDay();
+});
+
+function updateDay(){
+    // Obtén el elemento con el id "fechaActual"
+    let dateElement = document.getElementById("fechaActual");
+
+    // Crea un nuevo objeto Date para obtener la fecha actual
+    const fechaActual = new Date();
+    
+    // Formateamos la fecha para que se muestre de la siguiente manera: dia/mes/año
+    const day = String(fechaActual.getDate()).padStart(2, '0');  // Asegura que tenga 2 dígitos
+    const month = String(fechaActual.getMonth() + 1).padStart(2, '0');  // Suma 1 ya que los meses empiezan en 0
+    const year = fechaActual.getFullYear();
+    
+    const formatedDate = `${day}/${month}/${year}`;
+    
+    // Asignamos la fecha formateada al contenido HTML del elemento
+    dateElement.innerHTML = formatedDate;
+}
